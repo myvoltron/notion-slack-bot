228b0198a71e990593505732ee8e6c85407c4593
myvoltron <leejs8266@gmail.com>
2023-03-15 23:31:07 +0900

    feat: ConvertType으로 분기해서 메서드를 호출하는 메서드 추가

--- src/convertor/convertor.service.ts
+++ src/convertor/convertor.service.ts
@@ -1,4 +1,5 @@
 import { ApiMethodRow } from "../notion/notion.interface";
+import { ConvertType } from "./convertor.enum";
 
 export class ConvertorService {
   /** 배열을 markdown 테이블로 바꿉니다. **/
@@ -22,14 +23,24 @@ export class ConvertorService {
   }
 
   convertToSeeder(apiMethodArray: ApiMethodRow[]) {
-    return apiMethodArray.map((item, index) => ({
-      idx: index + 1,
-      type: item.type,
-      coin: {
-        symbol: item.type,
-      },
-      method: item.method,
-      ac: item.ac,
-    }));
+    return apiMethodArray
+      .map((item, index) => ({
+        idx: index + 1,
+        type: item.type,
+        coin: {
+          symbol: item.type,
+        },
+        method: item.method,
+        ac: item.ac,
+      }))
+      .toString();
+  }
+
+  convertApiMethodArray(apiMethodArray: ApiMethodRow[], type: ConvertType) {
+    if (type === ConvertType.MARKDOWN) {
+      return this.convertToMarkdown(apiMethodArray);
+    } else {
+      return this.convertToSeeder(apiMethodArray);
+    }
   }
 }
